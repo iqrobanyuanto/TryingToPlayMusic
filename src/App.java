bdf2afecb40d53e00dd93c70c758956771d8967e
Gapherion <iqrobanyuanto12345@gmail.com>
2023-02-01 00:20:15 +0700

    add the play music

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String musicPath = "Harumodoki(12000Hz).wav";
+        play(musicPath);
+        JOptionPane.showMessageDialog(null, "Press Ok to stop");
+    }
+
+    public static void play(String path){
+        try
+        {
+            File music = new File(path);
+            if(music.exists()){
+                AudioInputStream audio = AudioSystem.getAudioInputStream(music);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audio);
+                clip.start();
+            }else{
+                System.out.println("File Not Found");
+            }
+        }
+        catch(Exception s)  
+        {
+            System.out.println(s);
+        }
     }
 }
